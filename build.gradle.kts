eb86dcab383050dafab469ac7bbdb7b360c59daf
Martin Bonnin <martin@mbonnin.net>
2024-02-08 15:45:34 +0100

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
-  id("org.jetbrains.kotlin.multiplatform").version("1.9.20")
+  id("org.jetbrains.kotlin.jvm").version("1.9.20")
+  id("com.apollographql.apollo3").version("3.8.2")
 }
 
-kotlin {
-  jvm()
-  
-  sourceSets {
-    getByName("jvmTest") {
-      dependencies {
-        implementation(kotlin("test"))
-      }
+apollo {
+  service("confetti") {
+    packageName.set("confetti")
+
+    introspection {
+      endpointUrl.set("https://confetti-app.dev/graphql")
+      schemaFile.set(file("src/main/graphql/schema.graphqls"))
     }
   }
 }
